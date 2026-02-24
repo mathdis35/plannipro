33a1ae983b6029cf4e667fd9417755ff9a660448
mathdis35 <100351416+mathdis35@users.noreply.github.com>
2026-02-24 11:39:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -491,4 +491,5 @@ def telecharger(session_id, filename):
     return send_file(path, as_attachment=True, download_name=filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
